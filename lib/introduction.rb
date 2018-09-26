e4e02033a1e9191f29b36c5929eee7688922093d
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-26 04:56:06 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,8 @@
-def introduction(name, phrase)
+def introduction
   puts "Hi, I am #{name} and #{phrase}."
 end
+
 name= "Stephanie"
 phrase= "and I am learning the language of Ruby"
+
 puts introduction(name, phrase)
