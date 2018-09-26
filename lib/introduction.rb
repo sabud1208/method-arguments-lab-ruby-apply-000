40388bd25112fcf6519f017cf80ee170dabcb94b
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-26 05:00:49 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,14 @@
-def introduction(name, phrase)
-  puts "Hi, I am #{name} and #{phrase}."
+def introduction(name, age, phrase)
+  puts "Hi, I am #{name}, and #{phrase}."
 end
 
 name= "Stephanie"
 phrase= "and I am learning the language of Ruby"
-
+language= ruby
 puts introduction(name, phrase)
+
+def intro(name, language)
+  puts "Hi, I am #{name} and I am learning how to speak #{language}."
+end
+
+puts intro(name, language)
