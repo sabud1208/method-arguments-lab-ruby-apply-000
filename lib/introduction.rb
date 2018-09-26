1e856f74e9f474c9b3ea3fa40833ae1c00dc7b2b
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-26 05:23:15 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,11 +4,11 @@ end
 
 name= "Stephanie"
 language= "Ember"
-phrase= "I am learning to program"
+
 
 puts introduction(name)
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and #{phrase} in #{language}, it's cool."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}, it's cool."
 end
 
-puts introduction_with_language(name, phrase, language)
+puts introduction_with_language(name, language)
